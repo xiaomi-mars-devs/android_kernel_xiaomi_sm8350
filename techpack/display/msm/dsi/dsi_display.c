psndna88/AGNi_pureMIUI

techpack: display: Temporary disable dimlayer exposure while in high brightness mode

techpack/display/msm/dsi/dsi_display.c
@@ -252,7 +252,7 @@ int dsi_display_set_backlight(struct drm_connector *connector,
 
 #ifdef CONFIG_DRM_SDE_EXPO
 	if(panel->dimlayer_exposure) {
-		if (bl_lvl && !panel->doze_enabled) {
+		if (bl_lvl && !panel->doze_enabled && !panel->hbm_enabled) {
 			bl_temp = expo_map_dim_level((u32)bl_temp, dsi_display);
 		}
 	}

techpack/display/msm/sde/sde_crtc.c
@@ -2703,7 +2703,7 @@ static int sde_crtc_config_exposure_dim_layer(struct drm_crtc_state *crtc_state,
 		return -EINVAL;
 	}
 
-	if (!alpha || panel->doze_enabled) {
+	if (!alpha || panel->doze_enabled || panel->hbm_enabled) {
 		cstate->exposure_dim_layer = NULL;
 		return 0;
 	}
@@ -4870,7 +4870,7 @@ static int sde_crtc_exposure_atomic_check(struct sde_crtc_state *cstate,
 	struct dsi_display *dsi_display = get_main_display();
 	struct dsi_panel *panel = dsi_display->panel;
 
-	if (!panel->dimlayer_exposure || panel->doze_enabled) {
+	if (!panel->dimlayer_exposure || panel->doze_enabled || panel->hbm_enabled) {
 		cstate->exposure_dim_layer = NULL;
 		return 0;
 	}

xiaomi-mars-devs/android_kernel_xiaomi_sm8350

techpack/display/msm/dsi/dsi_display.c
@@ -257,7 +257,7 @@ int dsi_display_set_backlight(struct drm_connector *connector,
 
 #ifdef CONFIG_DRM_SDE_EXPO
 	if(panel->dimlayer_exposure) {
-		if (bl_lvl && !panel->doze_mode_requested) {
+		if (bl_lvl && !panel->doze_mode_requested && !panel->hbm_enabled) {
 			bl_temp = expo_map_dim_level((u32)bl_temp, dsi_display);
 		}
 	}

techpack/display/msm/sde/sde_crtc.c
@@ -2711,7 +2711,7 @@ static int sde_crtc_config_exposure_dim_layer(struct drm_crtc_state *crtc_state,
 		return -EINVAL;
 	}
 
-	if (!alpha || panel->doze_mode_requested) {
+	if (!alpha || panel->doze_mode_requested || panel->hbm_enabled) {
 		cstate->exposure_dim_layer = NULL;
 		return 0;
 	}
@@ -4889,7 +4889,7 @@ static int sde_crtc_exposure_atomic_check(struct sde_crtc_state *cstate,
 	struct dsi_display *dsi_display = get_main_display();
 	struct dsi_panel *panel = dsi_display->panel;
 
-	if (!panel->dimlayer_exposure || panel->doze_mode_requested) {
+	if (!panel->dimlayer_exposure || panel->doze_mode_requested || panel->hbm_enabled) {
 		cstate->exposure_dim_layer = NULL;
 		return 0;
 	}